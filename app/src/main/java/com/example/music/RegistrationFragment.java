735067991267a1ce5ca41ba4043973f4df9c67fa
Roberto070 <robert02erts@gmail.com>
2026-06-17 14:46:54 +0300

    Fix

--- app/src/main/java/com/example/music/RegistrationFragment.java
+++ app/src/main/java/com/example/music/RegistrationFragment.java
@@ -5,7 +5,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
 import com.example.music.databinding.RegistrationPageBinding;
 
 public class RegistrationFragment extends Fragment {
@@ -16,6 +19,14 @@ public class RegistrationFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = RegistrationPageBinding.inflate(inflater, container, false);
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.RegButton.setOnClickListener(v ->
+                Navigation.findNavController(v).navigate(R.id.action_registrationFragment_to_loginFragment));
     }
 
     @Override
