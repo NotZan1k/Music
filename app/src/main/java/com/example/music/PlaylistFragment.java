1770367e9a4f267b6ad6d92592954d3a5d05f870
Roberto070 <robert02erts@gmail.com>
2026-09-10 14:31:09 +0300

    Fix

--- app/src/main/java/com/example/music/PlaylistFragment.java
+++ app/src/main/java/com/example/music/PlaylistFragment.java
@@ -10,7 +10,7 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 import com.example.music.databinding.PlaylistDetailPageBinding;
 
-public class PlaylistFragment extends Fragment{
+public class PlaylistFragment extends Fragment {
 
     private PlaylistDetailPageBinding binding;
 
@@ -18,6 +18,23 @@ public class PlaylistFragment extends Fragment{
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = PlaylistDetailPageBinding.inflate(inflater, container, false);
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.playlistButtonHome.setOnClickListener(v -> 
+            Navigation.findNavController(v).navigate(R.id.action_playlistFragment_to_homeFragment)
+        );
+
+        binding.playlistButtonSearch.setOnClickListener(v -> 
+            Navigation.findNavController(v).navigate(R.id.action_playlistFragment_to_searchFragment)
+        );
+
+        binding.playlistButtonProfile.setOnClickListener(v -> 
+            Navigation.findNavController(v).navigate(R.id.action_playlistFragment_to_profileFragment)
+        );
     }
 
     @Override
