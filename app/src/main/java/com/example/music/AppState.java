7d4cc34af150b1b38ae3cbd60b51529a84b563cf
Roberto070 <robert02erts@gmail.com>
2026-09-17 11:14:47 +0300

    palīgklase

---
+++ app/src/main/java/com/example/music/AppState.java
@@ -0,0 +1,9 @@
+package com.example.music;
+
+public final class AppState {
+    public static long selectedPlaylistId = -1;
+    public static long selectedSongId = -1;
+
+    private AppState() {
+    }
+}
